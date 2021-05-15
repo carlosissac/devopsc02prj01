b5ba8a5464df37cb6f8745767150ff3156a009e1
carlosissac <issac.angulo@gmail.com>
2021-05-15 06:13:52 +0000

    Cambio 3 - Work

--- hola.java
+++ hola.java
@@ -1,5 +1,8 @@
-class hola {
-   public static void main(String[] args) {
-     System.out.println("Cambio de Codigo - Cambio 02 Espanol");
-   }
+public class hola {
+	String op = "Cambio de Codigo - Cambio 03 - Que funcione"; 
+
+   	public static void main(String[] args) {
+     		hola myObj = new hola();
+		System.out.println(myObj.op);
+   	}	
  }
