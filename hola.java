fbc272893311c0d80843b3e475ddcfa9aa514df8
carlosissac <issac.angulo@gmail.com>
2021-05-15 03:20:12 +0000

    Change 1 - Espanol

--- hola.java
+++ hola.java
@@ -1,5 +1,5 @@
 class Hola {
    public static void main(String[] args) {
-     System.out.println("Esta es una java app creada usando Docker - Change 01");
+     System.out.println("Esta es una java app creada usando Docker - Cambio 01 Espanol");
    }
  }
